69d190c87f9212e379134513bb25b214f3ebae76
liulei <5390128+liuad@user.noreply.gitee.com>
2020-07-02 09:13:31 +0800

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyBNBvOulC7Y9BG7umlFM0i3-15oIM4JcZk",
+  authDomain: "vuefs-prod-c7c5c.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-c7c5c.firebaseio.com",
+  projectId: "vuefs-prod-c7c5c",
+  storageBucket: "vuefs-prod-c7c5c.appspot.com",
+  messagingSenderId: "819685862427",
+  appId: "1:819685862427:web:92ed3eb29ba6b9f1e61cbe",
+  measurementId: "G-DH1D7TB7PK"
+};
---
+++ src/router/index.js
@@ -0,0 +1,103 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Dashboard from "@/components/Dashboard";
+import NewEmployee from "@/components/NewEmployee";
+import ViewEmployee from "@/components/ViewEmployee";
+import EditEmployee from "@/components/EditEmployee";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import firebase from "firebase";
+
+Vue.use(Router);
+
+let router = new Router({
+  routes: [
+    {
+      path: "/",
+      name: "Dashboard",
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: Register,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: "/new",
+      name: "NewEmployee",
+      component: NewEmployee,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/edit/:employee_id",
+      name: "EditEmployee",
+      component: EditEmployee,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/:employee_id",
+      name: "ViewEmployee",
+      component: ViewEmployee,
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  // Check for requiredAuth guard
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // Check if NOT logged in
+    if (!firebase.auth().currentUser) {
+      // Go to login
+      next({
+        path: "/login",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      //Proceed to route
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    // Check if logged in
+    if (firebase.auth().currentUser) {
+      // Go to login
+      next({
+        path: "/",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      //Proceed to route
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+// Nav Guards
+
+export default router;
